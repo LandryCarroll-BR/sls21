0ca6ce3fd388c7b6e064b136c8454525a9728d69
Landry Carroll <landry@boldist.co>
2023-07-27 08:19:53 -0400

    config: update GA4 measurement ID

--- src/components/site-head/site-head.tsx
+++ src/components/site-head/site-head.tsx
@@ -5,14 +5,14 @@ export function SiteHead({ children }: any) {
   return (
     <>
       <Head>{children}</Head>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID" />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-DVHXW75DC3" />
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
  
-          gtag('config', 'GA_MEASUREMENT_ID');
+          gtag('config', 'G-DVHXW75DC3');
         `}
       </Script>
     </>
